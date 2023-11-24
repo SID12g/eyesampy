d8285acdddd0b0c171d77b879ff70f20eba79c68
SID12g <sid12g@naver.com>
2023-11-25 00:06:34 +0900

    feat : ultrasonic wave

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import time
 import Adafruit_DHT as dht
 
 buttonPin = 26
+TRIGER = 24
+ECHO = 23
+
 
 g.setwarnings(False)
 
@@ -13,6 +16,8 @@ g.setup(21, g.OUT) # 위험 단계 : 위험 (빨간색)
 g.setup(buttonPin, g.IN, pull_up_down = g.PUD_DOWN) # 위험 단계를 초기화 시키는 버튼
 g.setup(12, g.OUT) # 위험 단계를 초기화 시키는 버튼 출력
 g.setup(17, g.OUT) # 온도와 습도를 감지
+g.setup(ECHO,g.IN) # 초음파 ECHO
+g.setup(TRIGER,g.OUT) # 초음파 TRIGER
 
 warnLevel = 1 # 1: 괜찮음 (혹은 정보 수집 전), 2: 주의, 3: 위험
 
@@ -33,4 +38,24 @@ while True:
     except KeyboardInterrupt:
         g.cleanup()
 
+    # 초음파 센서 부분   
+    g.output(TRIGER,g.LOW)
+    time.sleep(0.1)
+    g.output(TRIGER,g.HIGH)
+    time.sleep(0.00001)
+    g.output(TRIGER,g.LOW)
+    while g.input(ECHO) == g.LOW:
+        startTime = time.time()
+    while g.input(ECHO) == g.HIGH:
+        endTime = time.time()
+        
+    period = endTime - startTime
+    dist1 = round(period * 1000000/58,2)
+    dist2 = round(period * 17241,2)
+    if dist1 <= 10 and dist2 <= 10:
+        print("detect")
+    else:
+        print("no")
+
+       
 g.cleanup()

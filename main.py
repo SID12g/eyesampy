eb92cd96bacc3177a16ce7eecaf5036b0c04d5fe
SID12g <sid12g@naver.com>
2023-11-26 01:18:55 +0900

    aaa

--- main.py
+++ main.py
@@ -45,30 +45,21 @@ def ReadVol(vol):
 while True:
     brightness = ReadVol(0) # 밝기 정의
     # 코드 쓸 때 brightness < 100 이런 식으로 작성 if문 사용
-
     if warnLevel == 1:
-        print('1')
+        print('w1')
     elif warnLevel == 2:
-        print('2')
+        print('w2')
     elif warnLevel == 3:
-        print('3')
-
+        print('w3')
         # 위험 상태 도달 시 부저로 알림
         pwm.ChangeDutyCycle(50)
         pwm.ChangeFrequency(261) 
         time.sleep(1.0)
-
     if g.input(buttonPin) == g.HIGH:
         warnLevel = 1
         print("정상화")
-    try:
-        while True:
-            humidity, temperature = dht.read_retry(dht.DHT11,17) # 17번 핀으로 습도, 온도 감지, humidity = 습도, temperature = 온도
-    
-    # if 문으로 온습도에 따라서 단게 변경
 
-    except KeyboardInterrupt:
-        g.cleanup()
+
 
     # 초음파 센서 부분   
     g.output(TRIGER,g.LOW)
@@ -85,6 +76,17 @@ while True:
     dist1 = round(period * 1000000/58,2)
     dist2 = round(period * 17241,2)
 
+    try:
+        while True:
+            humidity, temperature = dht.read_retry(dht.DHT11,17) # 17번 핀으로 습도, 온도 감지, humidity = 습도, temperature = 온도
+    
+    # if 문으로 온습도에 따라서 단게 변경
+
+    except KeyboardInterrupt:
+        g.cleanup()
+
+    
+
     if 700>brightness:
         warnLevel = 2
     elif 100>brightness:
@@ -93,8 +95,10 @@ while True:
         warnLevel = 2
     elif 10>dist1:
         warnLevel = 3 
-
-    print(dist1)
+    print("temp : ", temperature)
+    print("humi : ", humidity)
+    print("dist:", dist1)
+    print("bright : ", brightness)
     print("warnLevel : ", warnLevel)
        
 g.cleanup()
